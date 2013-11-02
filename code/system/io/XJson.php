1eb23565654654cb38113f7a53a346c12698d402
Hyneman <rico.beti@silentbyte.com>
2013-11-03 00:39:34 +0800

    Set correct module name for XJson.php

--- code/system/io/XJson.php
+++ code/system/io/XJson.php
@@ -6,7 +6,7 @@
 ////// Copyright © 2013 by Silent Byte.
 //////
 ////// Author: Rico Beti (rico.beti@silentbyte.com)
-////// Module: index.php
+////// Module: system.io.XJson.php
 ////// 
 ////// Redistribution and use in source and binary forms, with or without
 ////// modification, are permitted provided that the following conditions are met:
