6ec0b88e5b4d2115acde7dcbd212a375a0226d42
Hyneman <rico.beti@silentbyte.com>
2013-12-02 00:28:40 +0800

    Corrected wrong class name in header comment.

--- code/system/database/DatabaseConnection.php
+++ code/system/database/DatabaseConnection.php
@@ -6,7 +6,7 @@
 ////// Copyright © 2013 by Silent Byte.
 //////
 ////// Author: Rico Beti (rico.beti@silentbyte.com)
-////// Module: system.database.MySqlDatabaseConnection.php
+////// Module: system.database.DatabaseConnection.php
 ////// 
 ////// Redistribution and use in source and binary forms, with or without
 ////// modification, are permitted provided that the following conditions are met:
