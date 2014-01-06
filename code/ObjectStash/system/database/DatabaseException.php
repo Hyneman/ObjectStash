8e7b22e48c2ac218f902a4fdc625e99f43fd25bd
Hyneman <rico.beti@silentbyte.com>
2014-01-06 21:19:44 +0800

    Added DatabaseException class.

---
+++ code/ObjectStash/system/database/DatabaseException.php
@@ -0,0 +1,86 @@
+<?php
+////
+//////
+//////   O B J E C T  S T A S H
+////// ==========================
+////// Copyright © 2013 by Silent Byte.
+//////
+////// Author: Rico Beti (rico.beti@silentbyte.com)
+////// Module: system.database.DatabaseException.php
+////// 
+////// Redistribution and use in source and binary forms, with or without
+////// modification, are permitted provided that the following conditions are met:
+//////     * Redistributions of source code must retain the above copyright
+//////       notice, this list of conditions and the following disclaimer.
+//////     * Redistributions in binary form must reproduce the above copyright
+//////       notice, this list of conditions and the following disclaimer in the
+//////       documentation and/or other materials provided with the distribution.
+//////     * Neither the name of the organization nor the names of its contributors
+//////       may be used to endorse or promote products derived from this software
+//////       without specific prior written permission.
+////// 
+////// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+////// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+////// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+////// DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+////// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+////// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+////// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+////// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+////// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+////// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//////
+////
+
+namespace system\database
+{
+	if(defined("OBJECT_STASH_AUTHORIZED") === false)
+		exit("UNAUTHORIZED ACCESS.");
+
+	use Exception;
+
+	class DatabaseException extends Exception
+	{
+		#region ...Member Variables...
+
+
+		private $databaseMessage;
+
+
+		#end region
+		#region ...Constructor...
+
+		public function __construct($message, $databaseMessage, $code = 0, Exception $inner = null)
+		{
+			parent::__construct($message, $code, $inner);
+			$this->databaseMessage = $databaseMessage;
+		}
+
+
+		#end region
+		#region ...Methods...
+
+
+		public function getDatabaseMessage()
+		{
+			return $this->databaseMessage;
+		}
+
+
+		#end region
+		#region ...Overrides...
+
+
+		public function __toString()
+		{
+			return __CLASS__ . ": " . $this->getMessage() . " "
+				. "(" . $this->getDatabaseMessage() . ")";
+		}
+
+
+		#end region
+	} // class DatabaseException extends Exception
+} // namespace system\database
+
+
+?>
